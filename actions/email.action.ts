bc2fb933b3667ed24d877a8946ae98a7dbd8106a
János Csordás <janoscsordas072@gmail.com>
2024-11-13 18:39:59 +0100

    Update email.action.ts

--- actions/email.action.ts
+++ actions/email.action.ts
@@ -3,10 +3,11 @@
 import { Resend } from "resend"
 import VerificationEmail from '@/emails/email-succesful';
 
-// Initialize Resend client
-const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function sendVerificationEmail(email: string, name: string, verificationLink: string) {
+    // Initialize Resend client
+    const resend = new Resend(process.env.RESEND_API_KEY)
+    
     await resend.emails.send({
         from: "no-reply@planitapp.hu",
         to: email,
