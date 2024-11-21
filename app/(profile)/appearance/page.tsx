a03a627832113d11cc48d1f119a9245756b804fd
AndrisVasari <andrasvasari47@gmail.com>
2024-11-21 10:59:27 +0100

    Updated profile page and appearance page.

--- app/(profile)/appearance/page.tsx
+++ app/(profile)/appearance/page.tsx
@@ -1,70 +1,52 @@
 import ProfileNavbar from "@/components/profile/ProfileNavbar";
+import { Button } from "@/components/ui/button";
+import { Flex, RadioCards, Text } from "@radix-ui/themes";
+import Image from "next/image";
 
 export default function AppearancePage() {
   return (
-    <div className="w-[90%] mx-auto pt-16">
+    <div className="w-[90%] mx-auto pt-8 md:pt-16">
       <h2 className="text-2xl font-bold">Fiókbeállítások</h2>
       <p className="text-muted-foreground pt-2">
         Fiókbeállítások kezelése és testreszabása.
       </p>
       <hr className="my-6" />
-      <div className="flex">
+      <div className="flex flex-col md:flex-row">
         <ProfileNavbar />
-        <div className="ml-12 mt-2 w-[50%]">
+        <div className="ml-0 md:ml-12 mt-2 w-full md:w-[50%]">
           <h3 className="text-xl font-medium">Megjelenés</h3>
           <p className="text-muted-foreground text-sm pt-2">
             Itt testreszabhatod az alkalmazás megjelenését.
           </p>
           <hr className="my-6" />
           <h4 className="font-medium text-sm">Téma beállítása</h4>
-          <p className="text-muted-foreground text-xs pt-2">
+          <p className="text-muted-foreground text-xs pt-2 mb-3">
             Válaszd ki az alkalmazás témáját.
           </p>
 
-          <div className="flex gap-10 mt-2">
-            <div>
-              <input type="radio" className="hidden"  />
-              <div
-                className={`items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground`}
-              >
-                <div className="space-y-2 rounded-sm bg-[#ecedef] p-2">
-                  <div className="space-y-2 rounded-md bg-white p-2 shadow-sm">
-                    <div className="h-2 w-[80px] rounded-lg bg-[#ecedef]"></div>
-                    <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]"></div>
-                  </div>
-                  <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
-                    <div className="h-4 w-4 rounded-full bg-[#ecedef]"></div>
-                    <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]"></div>
-                  </div>
-                  <div className="flex items-center space-x-2 rounded-md bg-white p-2 shadow-sm">
-                    <div className="h-4 w-4 rounded-full bg-[#ecedef]"></div>
-                    <div className="h-2 w-[100px] rounded-lg bg-[#ecedef]"></div>
-                  </div>
-                </div>
-              </div>
-              <h4 className="text-sm text-center pt-2">Világos</h4>
-            </div>
-            <div>
-              <div className="items-center rounded-md border-2 border-muted bg-popover p-1 hover:bg-accent hover:text-accent-foreground">
-                <input type="radio" className="hidden" />
-                <div className="space-y-2 rounded-sm bg-slate-950 p-2">
-                  <div className="space-y-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                    <div className="h-2 w-[80px] rounded-lg bg-slate-400"></div>
-                    <div className="h-2 w-[100px] rounded-lg bg-slate-400"></div>
-                  </div>
-                  <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                    <div className="h-4 w-4 rounded-full bg-slate-400"></div>
-                    <div className="h-2 w-[100px] rounded-lg bg-slate-400"></div>
-                  </div>
-                  <div className="flex items-center space-x-2 rounded-md bg-slate-800 p-2 shadow-sm">
-                    <div className="h-4 w-4 rounded-full bg-slate-400"></div>
-                    <div className="h-2 w-[100px] rounded-lg bg-slate-400"></div>
-                  </div>
-                </div>
-              </div>
-              <h4 className="text-sm text-center pt-2">Sötét</h4>
-            </div>
-          </div>
+          <form className="flex flex-col gap-10 mt-2 w-full">
+            <RadioCards.Root defaultValue="light" color="green" columns={{ initial: "1", md: "2", lg: "3" }}>
+              <RadioCards.Item value="light" className="">
+                <Flex direction="column">
+                  <Image className="rounded-md" src="/lightMode.png" alt="Világos" width={180} height={180} />
+                  <h4 className="text-sm text-center pt-2">Világos</h4>
+                </Flex>
+              </RadioCards.Item>
+              <RadioCards.Item value="dark" className="">
+                <Flex direction="column">
+                  <Image className="rounded-md" src="/darkMode.png" alt="Sötét" width={180} height={180} />
+                  <h4 className="text-sm text-center pt-2">Sötét</h4>
+                </Flex>
+              </RadioCards.Item>
+              <RadioCards.Item value="system" className="">
+                <Flex direction="column">
+                  <Image className="rounded-md" src="/darkMode.png" alt="Rendszer" width={180} height={180} />
+                  <h4 className="text-sm text-center pt-2">Rendszer</h4>
+                </Flex>
+              </RadioCards.Item>
+            </RadioCards.Root>
+            <Button className="mb-10 bg-emerald hover:bg-emerald-hover w-fit" type="submit">Változtatások mentése</Button>
+          </form>
         </div>
       </div>
     </div>
--- app/(profile)/profile/page.tsx
+++ app/(profile)/profile/page.tsx
@@ -6,7 +6,7 @@ import { DatePicker } from "@/components/profile/DatePicker";
 export default function ProfilePage() {
 
     return (
-        <div className="w-[90%] mx-auto pt-16">
+        <div className="w-[90%] mx-auto pt-8 md:pt-16">
             <h2 className="text-2xl font-bold">
                 Fiókbeállítások
             </h2>
@@ -14,30 +14,32 @@ export default function ProfilePage() {
                 Fiókbeállítások kezelése és testreszabása.
             </p>
             <hr className="my-6" />
-            <div className="flex">
+            <div className="flex flex-col md:flex-row">
                 <ProfileNavbar />
-                <div className="ml-12 mt-2 w-[50%]">
+                <div className="ml-0 md:ml-12 mt-2 w-full md:w-[50%]">
                     <h3 className="text-xl font-medium">Profilom</h3>
                     <p className="text-muted-foreground text-sm pt-2">Ahogyan mások látnak téged az oldalon.</p>
                     <hr className="my-6" />
-                    <form action="">
+                    <form action="" className="border border-muted rounded-md p-4">
                         <h4 className="font-medium text-sm">Felhasználónév</h4>
-                        <Input type="text" placeholder="Felhasználónév" className="mt-2" />
+                        <Input className="mt-2 text-sm" type="text" placeholder="Felhasználónév" />
                         <p className="text-muted-foreground text-xs pt-2">Ez a nyílvános felhasználóneved. Csak 30 naponta módosítható.</p>
                         <h4 className="font-medium text-sm mt-10">Születési dátum</h4>
                         <DatePicker />
                         <p className="text-muted-foreground text-xs pt-2">Válaszd ki a születési dátumod.</p>
+                        <Button className="mb-10 md:mb-0 mt-10 bg-emerald hover:bg-emerald-hover" type="submit">Mentés</Button>
                     </form>
-                    <form action="">
-                        <h4 className="font-medium text-sm mt-10">Profilkép</h4>
+                    <form action="" className="border border-muted rounded-md p-4 mt-6">
+                        <h4 className="font-medium text-sm">Profilkép</h4>
                         <Input id="file" type="file" className="mt-2" />
+                        <Button className="mb-10 md:mb-0 mt-10 bg-emerald hover:bg-emerald-hover" type="submit">Mentés</Button>
                     </form>
-                    <form action="">
-                        <h4 className="font-medium text-sm mt-10">Jelszó</h4>
-                        <Input className="mt-2" type="password" placeholder="Új jelszó" />
-                        <Input className="mt-2" type="password" placeholder="Új jelszó megerősítése" />
+                    <form action="" className="border border-muted rounded-md p-4 mt-6">
+                        <h4 className="font-medium text-sm">Jelszó</h4>
+                        <Input className="mt-2 text-sm" type="password" placeholder="Új jelszó" />
+                        <Input className="mt-2 text-sm" type="password" placeholder="Új jelszó megerősítése" />
+                        <Button className="mb-10 md:mb-0 mt-10 bg-emerald hover:bg-emerald-hover" type="submit">Mentés</Button>
                     </form>
-                    <Button className="mt-10 bg-emerald hover:bg-emerald-hover">Változtatások mentése</Button>
                 </div>
             </div>
         </div>
