815333e5600f90184bb6323d4b4bf262f4a3958a
Koós Balázs Bence <koosbalazsbence@gmail.com>
2024-12-03 08:50:29 +0100

    Update sidebar and add component

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<html lang="hu" className="dark">
+		<html lang="hu" className="light">
 			<body className={`${GeistSans.className} antialiased`}>
 				<Theme hasBackground={false}>
 					<Providers>{children}</Providers>
