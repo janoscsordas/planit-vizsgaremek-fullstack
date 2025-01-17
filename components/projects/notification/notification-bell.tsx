d1d7cb5e8511bcdf0e1b6f026366b8e6eda26083
János Csordás <janoscsordas072@gmail.com>
2025-01-17 15:36:22 +0100

    updated notification bell component

--- components/projects/notification/notification-bell.tsx
+++ components/projects/notification/notification-bell.tsx
@@ -1,7 +1,7 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
 import { useNotifications } from "@/hooks/useNotifications"
-import { IconButton } from "@radix-ui/themes"
 import { Bell } from "lucide-react"
 import Link from "next/link"
 
@@ -14,19 +14,19 @@ export default function NotificationBell() {
       className="flex items-center justify-center"
       aria-label="Értesítések"
     >
-      <IconButton
-        variant="ghost"
-        color="gray"
+      <Button
+        variant="outline"
+        size="icon"
         className="relative cursor-pointer group hover:bg-emerald/15"
         aria-label="Értesítések"
       >
-        <Bell width="18" height="18" className="text-muted-foreground" />
+        <Bell width="20" height="20" className="text-muted-foreground" />
         {notifications.length > 0 && (
-          <span className="absolute top-0 right-0 w-4 h-4 text-xs text-white transform translate-x-1/2 -translate-y-1/2 bg-red-500 rounded-full">
+          <p className="absolute top-0 right-0 w-4 h-4 text-xs text-white transform translate-x-1/2 -translate-y-1/2 bg-red-500 rounded-full">
             {notifications.length > 9 ? "9+" : notifications.length}
-          </span>
+          </p>
         )}
-      </IconButton>
+      </Button>
     </Link>
   )
 }
